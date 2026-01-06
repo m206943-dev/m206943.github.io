755df709572fbc340402721ef087bf824092b9c1
m206943-dev <m206943@dac.unicamp.br>
2026-01-06 21:54:09 +0100

    Create Connecter.vue

---
+++ frontend/src/pages/Connecter.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<h1>Connexion</h1>
+<p>Connexion sécurisée via Enedis</p>
+<p>Nous ne demandons jamais votre mot de passe Enedis</p>
+<p>Consentement révocable à tout moment</p>
+<button @click="connect">Se connecter avec Enedis</button>
+</div>
+</template>
+
+
+<script setup>
+function connect() {
+window.location.href = `${import.meta.env.VITE_BACKEND_URL}/auth/enedis/start`
+}
+</script>
