4bdb55e33e05cac123ef98b96d30f7e0bafcf781
m206943-dev <m206943@dac.unicamp.br>
2026-01-06 21:55:05 +0100

    Create Navbar.vue

---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+<nav>
+<a href="/">Home</a>
+<a href="/qui-sommes-nous">Qui sommes-nous</a>
+<a href="/projet">Projet</a>
+<a href="/rgpd">RGPD</a>
+<a href="/connecter">Connecter</a>
+</nav>
+</template>
+
+
+<style scoped>
+nav {
+display: flex;
+gap: 1rem;
+padding: 1rem;
+}
+</style>
